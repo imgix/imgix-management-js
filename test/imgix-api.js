2c8cc7d54e3946065fd5f3dfbb549cea4a61c5d3
sherwinski <sherwin@imgix.com>
2020-04-17 16:36:06 -0700

    test: add unit tests (#1)

---
+++ test/imgix-api.js
@@ -0,0 +1,32 @@
+var ImgixAPI = require('../src/imgix-api');
+
+var assert = require('assert');
+
+describe('The imgix API class', function describeSuite() {
+    it('creates an instance of the class', function testSpec() {
+        var ix = new ImgixAPI();
+        assert(ix);
+    });
+
+    it('is created with a default version value', function testSpec() {
+        var ix = new ImgixAPI();
+        assert(ix.settings.version);
+    });
+
+    it('allows the version value to be set', function testSpec() {
+        var ix = new ImgixAPI({version: 2});
+        assert(ix.settings.version == 2);
+    });
+});
+
+describe('ImgixAPI.prototype.request', function describeSuite() {
+    var ix;
+
+    beforeEach(function setupClient() {
+        ix = new ImgixAPI();
+    });
+
+    it('exposes a method request() on the object prototype', function testSpec() {
+        assert(typeof ix.request === 'function');
+    });
+});
