f5197c7ad970a263e4c30dfbdc1e809aa61cadca
Sherwin H <sherwin@imgix.com>
2020-04-20 16:34:58 -0700

    feat: uses WindowOrWorkerGlobalScope.fetch() when in a browser env

--- test/imgix-api.js
+++ test/imgix-api.js
@@ -14,18 +14,32 @@ describe('The imgix API class', () => {
 
     it('allows the version value to be set', () => {
         var ix = new ImgixAPI({version: 2});
-        assert(ix.settings.version == 2);
+        assert.equal(ix.settings.version, 2);
+    });
+
+    it('fetch exists in the global namespace as window.fetch', function testSpec() {
+        assert.throws(() => assert(window.fetch), Error);
     });
 });
 
 describe('ImgixAPI.prototype.request', () => {
     var ix;
 
-    beforeEach(function setupClient() {
+    before(function setupClient() {
         ix = new ImgixAPI();
     });
 
     it('exposes a method request() on the object prototype', () => {
-        assert(typeof ix.request === 'function');
+        assert.equal(typeof ix.request, 'function');
+    });
+
+    it('calling request() returns a response as a Promise', () => {
+        const req1 = ix.request("https://api.imgix.com/api/v1/assets")
+        .then(response => {
+            assert.exists(response.headers);
+        })
+        .catch(() => {});
+
+        assert.equal(typeof req1.then, 'function');
     });
 });
