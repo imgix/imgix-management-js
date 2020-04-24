25bf57882f6c73b8a9ed7f13237dd4ad0e881caf
Sherwin H <sherwin@imgix.com>
2020-04-23 19:24:54 -0700

    feat: allow API key to be configured at object instantiation

--- src/imgix-api.js
+++ src/imgix-api.js
@@ -1,20 +1,26 @@
 (function (global, factory) {
     if (typeof define === 'function' && define.amd) {
-        define('ImgixAPI', ['exports', './fetchWrapper'], factory);
+        // AMD. Register as an anonymous module.
+        define('ImgixAPI', ['exports', './fetchWrapper', './validators'], factory);
     } else if (typeof exports !== 'undefined') {
-        module.exports = factory(exports, require('./fetchWrapper'));
+        // CommonJS
+        module.exports = factory(exports, require('./fetchWrapper'), require('./validators'));
     } else {
+        // Browser globals
         var mod = {
             exports: {
                 window
             }
         };
-        global.ImgixAPI = factory(mod.exports, global.fetchWrapper);
+        global.ImgixAPI = factory(mod.exports, global.fetchWrapper, global.validators);
     }
-})(this, function (exports, fetchWrapper) {
+})(this, function (exports, fetchWrapper, validators) {
     'use strict';
-    let fetch;
+    const { validateOpts } = validators;
 
+    // Depending on loading environment, either use
+    // window.fetch or node-fetch to complete requests
+    let fetch;
     if (exports.window !== undefined) {
         fetch = window.fetch;
     } else {
@@ -23,18 +29,14 @@
 
     // default ImgixAPI settings passed in during instantiation
     const DEFAULTS = {
-        version: 1
+        version: 1,
+        apiKey: null
     };
 
     const ImgixAPI = (() => {
         function ImgixAPI(opts = {}) {
-            this.settings = {};
-
-            if (opts.version) {
-                this.settings.version = opts.version;
-            } else {
-                this.settings = DEFAULTS;
-            }
+            validateOpts(opts);
+            this.settings = Object.assign({}, DEFAULTS, opts);
         };
 
         return ImgixAPI;
@@ -42,8 +44,11 @@
 
     ImgixAPI.prototype.request = function(path, options = {}) {
         return fetch(path, {
+            version: this.settings.version,
+            headers: {
+                'Authorization': `apikey ${this.settings.apiKey}`,
+            },
             ...options,
-            version: this.settings.version
         });
     };
 
