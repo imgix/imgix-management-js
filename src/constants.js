45eb89dbeaf4fee67cf9b0222f57fed76deec183
Sherwin H <sherwin@imgix.com>
2020-05-04 13:16:43 -0700

    refactor: define src and test constants

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,7 @@
-const VERSION = require('../package-lock.json').version;
+const PACKAGE_VERSION = require('../package-lock.json').version;
+const API_URL = 'https://api.imgix.com/api';
 
 module.exports = {
-    VERSION: VERSION
+    PACKAGE_VERSION: PACKAGE_VERSION,
+    API_URL: API_URL
 }
