50df2e2e4e428fb23dffbe9f49d4a500ca44302b
Sherwin H <sherwin@imgix.com>
2020-05-06 12:19:55 -0700

    refactor: fetchWrapper
    - splits into two modules: fetch-wrapper and api-error
    - renames files using kebab-case
    - pull logic from imgix-api to determine loading env into fetch-wrapper

---
+++ src/api-error.js
@@ -0,0 +1,23 @@
+// Custom API error to throw
+function APIError(message, data, status) {
+  let response = null;
+  let isObject = false;
+
+  try {
+    response = JSON.parse(data);
+    isObject = true;
+  } catch (e) {
+    response = data;
+  }
+
+  this.response = response;
+  this.message = message;
+  this.status = status;
+
+  this.toString = () =>
+    `${this.message}\nResponse:\n${
+      isObject ? JSON.stringify(this.response, null, 2) : this.response
+    }`;
+}
+
+module.exports = APIError;
