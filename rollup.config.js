0d8a89cac3d76a0409aa7e3e4d1932faec871047
sherwinski <sherwin@imgix.com>
2020-09-23 22:19:53 -0700

    config(rollup): add plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,46 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import builtins from 'builtin-modules';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
+import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+
 export default [
     {
         input: 'src/imgix-api.js',
-        output: [
-            {
-                file: 'dist/imgix-api.cjs.js',
-                format: 'cjs'
-            },
-            {
-                file: 'dist/imgix-api.umd.js',
-                format: 'umd',
-                name: 'ImgixAPI'
+        output:
+        {
+            file: 'dist/imgix-api.cjs.js',
+            format: 'cjs'
+        },
+        external: [
+            'assert',
+            'node-fetch'
+        ],
+        plugins: [
+            getBabelOutputPlugin({
+                presets: ['@babel/preset-env']
+            })
+        ],
+        exports: 'default'
+    },
+    {
+        input: 'src/imgix-api.js',
+        output:
+        {
+            file: 'dist/imgix-api.umd.js',
+            format: 'umd',
+            name: 'ImgixAPI',
+            globals: {
+                'node-fetch': 'nodeFetch'
             }
+        },
+        external: [
+            'node-fetch'
+        ],
+        plugins: [
+            nodePolyfills(),
+            // nodeResolve(),
+            // commonjs()
         ]
     },
     {
@@ -25,14 +55,19 @@ export default [
         output: {
             file: 'dist/validators.cjs.js',
             format: 'cjs'
-        }
+        },
+        external: [ 'assert' ]
     },
     {
         input: 'src/fetch-wrapper.js',
         output: {
             file: 'dist/fetch-wrapper.cjs.js',
             format: 'cjs'
-        }
+        },
+        external: [ 'node-fetch' ],
+        plugins: [
+            nodePolyfills()
+        ]
     },
     {
         input: 'src/constants.js',
