9426f75064d16107fe849ea879dc07e82de933bb
sherwinski <sherwin@imgix.com>
2021-03-25 14:12:01 -0700

    feat: use more explicitly-defined JSON types

--- src/imgix-api.d.ts
+++ src/imgix-api.d.ts
@@ -1,11 +1,16 @@
 // Type definitions for imgix-management-js
-import { RequestInfo, RequestInit } from "@types/node-fetch";
+import { RequestInit } from "@types/node-fetch";
+
+// JSON types via https://github.com/microsoft/TypeScript/issues/1897#issuecomment-338650717
+type AnyJson = boolean | number | string | null | JsonArray | JsonMap;
+interface JsonMap {  [key: string]: AnyJson; }
+interface JsonArray extends Array<AnyJson> {}
 
 interface ImgixResponse {
-    data : {}
-    included: {}
-    jsonapi: {}
-    meta: {}
+    data: JsonMap | JsonArray;
+    included: JsonArray;
+    jsonapi: JsonMap;
+    meta: JsonMap;
 }
 
 declare class ImgixAPI {
