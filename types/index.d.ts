d67409aeebe94e95ef9bbf6b5554be9738c1f2c5
sherwinski <sherwin@imgix.com>
2021-03-25 22:44:30 -0700

    style: sync with prettier

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,44 +1,43 @@
 /// <reference types="typescript" />
-import { RequestInit, BodyInit } from "node-fetch";
+import { RequestInit, BodyInit } from 'node-fetch';
 
 // JSON types via https://github.com/microsoft/TypeScript/issues/1897#issuecomment-338650717
 type AnyJson = boolean | number | string | null | JsonArray | JsonMap;
-interface JsonMap {  [key: string]: AnyJson; }
+interface JsonMap {
+  [key: string]: AnyJson;
+}
 interface JsonArray extends Array<AnyJson> {}
 
 interface RequestResponse {
-    data: JsonMap | JsonArray;
-    included: JsonArray;
-    jsonapi: JsonMap;
-    meta: JsonMap;
+  data: JsonMap | JsonArray;
+  included: JsonArray;
+  jsonapi: JsonMap;
+  meta: JsonMap;
 }
 
 interface RequestError {
-    response: JsonMap;
-    message: string;
-    status: number;
-    toString(): string;
+  response: JsonMap;
+  message: string;
+  status: number;
+  toString(): string;
 }
 
 interface JsonBody {
-    body?: BodyInit | JsonMap
+  body?: BodyInit | JsonMap;
 }
 
 type RequestOptions = RequestInit | JsonBody;
 
 declare class ImgixAPI {
-    apiKey: string;
-    version: number;
+  apiKey: string;
+  version: number;
 
-    constructor(opts: {
-        apiKey: string;
-        version?: number;
-    });
+  constructor(opts: { apiKey: string; version?: number });
 
-    request(
-        path: string,
-        options?: RequestOptions,
-    ): Promise<RequestResponse | RequestError>
+  request(
+    path: string,
+    options?: RequestOptions,
+  ): Promise<RequestResponse | RequestError>;
 }
 
 export = ImgixAPI;
