da517913e3c14b1b22d58e34744ad28459ca56fd
sherwinski <sherwin@imgix.com>
2021-03-26 12:11:48 -0700

    refactor: APIError to extend Error interface

--- src/api-error.js
+++ src/api-error.js
@@ -1,23 +1,27 @@
-// Custom API error to throw
-function APIError(message, data, status) {
-  let response = null;
-  let isObject = false;
+class APIError extends Error {
+  constructor(message, data, status) {
+    super(message);
+    this.name = 'APIError';
+    let response = null;
+    this.isObject = false;
 
-  try {
-    response = JSON.parse(data);
-    isObject = true;
-  } catch (e) {
-    response = data;
+    try {
+      response = JSON.parse(data);
+      this.isObject = true;
+    } catch (e) {
+      response = data;
+    }
+
+    this.response = response;
+    this.message = message;
+    this.status = status;
   }
 
-  this.response = response;
-  this.message = message;
-  this.status = status;
-
-  this.toString = () =>
-    `${this.message}\nResponse:\n${
-      isObject ? JSON.stringify(this.response, null, 2) : this.response
+  toString() {
+    return `${this.message}\nResponse:\n${
+      this.isObject ? JSON.stringify(this.response, null, 2) : this.response
     }`;
+  }
 }
 
 module.exports = APIError;
