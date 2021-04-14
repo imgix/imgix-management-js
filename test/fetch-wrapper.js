7a640a157dfcac3dbc63e3b4ae0f0b9996d1ba87
sherwinski <sherwin@imgix.com>
2021-04-13 22:04:30 -0400

    test: use assert.strictEqual over assert.equal

--- test/fetch-wrapper.js
+++ test/fetch-wrapper.js
@@ -5,10 +5,10 @@ const assert = require('assert');
 
 describe('fetchWrapper', () => {
   it('exports a function: fetch', () => {
-    assert.equal(typeof fetch, 'function');
+    assert.strictEqual(typeof fetch, 'function');
   });
 
   it('fetch returns a reference to node-fetch in a node.js env', () => {
-    assert.equal(fetch, nodeFetch);
+    assert.strictEqual(fetch, nodeFetch);
   });
 });
